4134c4073a9ffdfcb18af3fd108fb066a95f8e5d
Justin Reeves <reeves122@gmail.com>
2023-03-09 16:36:02 -0600

    go fmt

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -2,8 +2,8 @@ package test
 
 import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"testing"
 	"os"
+	"testing"
 )
 
 // TestExamplesComplete tests a typical deployment of this module
@@ -11,13 +11,13 @@ func TestExamplesComplete(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := &terraform.Options{
-		TerraformDir:  "../../examples/complete",
+		TerraformDir: "../../examples/complete",
 		BackendConfig: map[string]interface{}{
 			"bucket": os.Getenv("TF_STATE_BUCKET"),
 			"key":    os.Getenv("TF_VAR_git"),
 		},
-		EnvVars:       map[string]string{},
-		Vars:          map[string]interface{}{},
+		EnvVars: map[string]string{},
+		Vars:    map[string]interface{}{},
 	}
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
