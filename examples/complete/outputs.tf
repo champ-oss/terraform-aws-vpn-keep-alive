f0ae574214622026866e219bd628a54a6a8a0caa
Justin Reeves <reeves122@gmail.com>
2023-03-10 09:27:35 -0600

    update integration test to check log output

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,8 @@
+output "cloudwatch_log_group" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group"
+  value       = module.this.cloudwatch_log_group
+}
+
 output "region" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/region#name"
   value       = data.aws_region.current.name
